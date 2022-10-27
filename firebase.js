834bd22df53c5a9944a622714ab13d365f308026
Trevor Dooley <98045078+trevorddooley@users.noreply.github.com>
2022-10-26 19:56:42 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLA4vnLFaVow25ylLUkb57NhmnN6qk1vE",
-  authDomain: "fir-auth-65294.firebaseapp.com",
-  projectId: "fir-auth-65294",
-  storageBucket: "fir-auth-65294.appspot.com",
-  messagingSenderId: "105428798939",
-  appId: "1:105428798939:web:49a9375f32ea94d4dfebfe"
+  apiKey: "AIzaSyCTTEIXbO0fQKCyLMsWR6VDvgvRyeBS1Ic",
+  authDomain: "apcs360-9ac59.firebaseapp.com",
+  projectId: "apcs360-9ac59",
+  storageBucket: "apcs360-9ac59.appspot.com",
+  messagingSenderId: "830189127711",
+  appId: "1:830189127711:web:82efddaa5f96beb5c8ad2a"
 };
 
 // Initialize Firebase
