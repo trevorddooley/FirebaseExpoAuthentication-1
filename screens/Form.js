66b4249501c7a261baa1f194864de189efb0fc22
Trevor Dooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-01 13:58:35 -0500

    Update Form.js

--- screens/Form.js
+++ screens/Form.js
@@ -1,128 +1,78 @@
-import { useNavigation } from '@react-navigation/core'
-import React, { useEffect, useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { auth } from '../firebase'
+import React, {useState} from 'react';
+//import * as React from 'react';
+import {View, Alert} from 'react-native';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import { StyleSheet, Text, SafeAreaView, Dimensions  } from 'react-native';
+import MapView from 'react-native-maps';
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const navigation = useNavigation()
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        navigation.replace("Home")
-      }
-    })
-
-    return unsubscribe
-  }, [])
-
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(userCredentials => {
-        const user = userCredentials.user;
-        console.log('Registered with:', user.email);
-      })
-      .catch(error => alert(error.message))
-  }
-
-  const handleLogin = () => {
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then(userCredentials => {
-        const user = userCredentials.user;
-        console.log('Logged in with:', user.email);
-      })
-      .catch(error => alert(error.message))
-  }
-
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding"
-    >
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        />
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={handleLogin}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={handleSignUp}
-          style={[styles.button, styles.buttonOutline]}
-        >
-          <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  )
-}
-
-export default LoginScreen
+const STYLES = ['default', 'dark-content', 'light-content'];
+const TRANSITIONS = ['fade', 'slide', 'none'];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  inputContainer: {
-    width: '80%'
-  },
-  input: {
-    backgroundColor: 'white',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  buttonContainer: {
-    width: '60%',
     justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
   },
-  button: {
-    backgroundColor: '#0782F9',
+  map: {
     width: '100%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
+    height: '100%',
   },
-  buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  buttonOutlineText: {
-    color: '#0782F9',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-})
+});
+
+function MemberSign({navigation}) {
+  const [userName, setAnimalSpecies] = useState(null);
+  const [userSurName, setAnimalAge] = useState(null);
+  const [userAge, setUserAge] = useState(null);
+  const [userMail, setUserMail] = useState(null);
+
+  function handleSubmit() {
+    if (!userName || !userSurName || !userAge || !userMail) {
+      Alert.alert('Tiger Fitness Salonu', 'Bilgiler boş bırakılamaz!');
+      return;
+    }
+
+    const animal = {
+      animalSpecies,
+      animalAge,
+      userAge,
+      userMail,
+    };
+    navigation.navigate('MemberResultScreen', {animal});
+  }
+
+  return (
+<SafeAreaView style={{ flex: 1 }}>
+      {/* <Text>Member Sign</Text> */}
+      <View style={styles.container}>
+        <MapView style={styles.map} />
+      </View>
+      
+      <Input
+        label="Species"
+        placeholder="Insert species..."
+        onChangeText={setAnimalSpecies}
+      />
+      <Input
+        label="Age"
+        placeholder="Approximate age..."
+        onChangeText={setAnimalAge}
+      />
+      <Input
+        label="Date and Time"
+        placeholder="Select the date and time..."
+        onChangeText={setUserAge}
+      />
+      <Input
+        label="Deceases?"
+        placeholder="Yes or No..."
+        onChangeText={setUserMail}
+      />
+      <Button text="Complete Submission" onPress={handleSubmit} />
+    </SafeAreaView>
+  );
+}
+
+export default MemberSign;
