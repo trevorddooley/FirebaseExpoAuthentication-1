d626704d8ad036628b51f84bcd22279da51718bf
Trevor Dooley <98045078+trevorddooley@users.noreply.github.com>
2022-11-01 14:49:52 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import MemberSign from './screens/Form';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MemberSign" component={Form} />
       </Stack.Navigator>
     </NavigationContainer>
   );
